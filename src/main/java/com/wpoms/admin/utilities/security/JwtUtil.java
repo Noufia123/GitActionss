d667fcf1b9df20a0a69d67b469318a940b1dc8cd
Alan Siby <imalansiby@gmail.com>
2026-04-17 12:47:59 +0530

    Created product and vendor staff payload and response along with expiry updation on jwtutil file.

--- src/main/java/com/wpoms/admin/utilities/security/JwtUtil.java
+++ src/main/java/com/wpoms/admin/utilities/security/JwtUtil.java
@@ -20,7 +20,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 6))
                 .signWith(key)
                 .compact();
     }
