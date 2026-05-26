d5eafad230af3c7f29fc713b1bbe1a89165ab62e
Noufia123 <noufiyamohammed84@gmail.com>
2026-05-26 11:49:08 +0530

    secret add

--- src/main/java/com/wpoms/admin/controllers/LoginController.java
+++ src/main/java/com/wpoms/admin/controllers/LoginController.java
@@ -8,7 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+                                                
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
